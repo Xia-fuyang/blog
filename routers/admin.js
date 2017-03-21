267179e33fe417b4709bb9625acc682b96131ecb
Xia-fuyang <1427307634@qq.com>
2017-03-21 21:55:56 +0800

    关于内容的更新
    实现了内容的增删改

--- routers/admin.js
+++ routers/admin.js
@@ -5,6 +5,7 @@ var key = "xiafuyang*1995year";
 
 var User = require("../models/User");
 var Category = require("../models/Category");
+var Content = require("../models/Content");
 
 router.use(function(req,res,next) {
     if(!req.userInfo.isAdmin) {
@@ -100,7 +101,11 @@ router.get("/category", function(req, res) {
         for(var i = 1; i <= pages; i++){
             a.push('{'+i+'}');
         }
-        Category.find().limit(limit).skip(skip).then(function(categories) {
+        /*
+        * 1:升序
+        * -1:降序
+        * */
+        Category.find().sort({_id: -1}).limit(limit).skip(skip).then(function(categories) {
 
             res.render("admin/category_index",{
                 userInfo: req.userInfo,
@@ -264,5 +269,168 @@ router.get("/category/delete", function(req, res) {
     })
 });
 
+/*
+* 内容首页
+* */
+router.get("/content", function(req, res) {
+
+    var page = Number(req.query.page || 1);
+    var limit = 10;
+    var pages = 0;
+    var a = [];
+
+    Content.count().then(function(count) {
+
+        // 计算总页数
+        pages = Math.ceil(count / limit);
+        // 取值不能超过pages
+        page = Math.min(page, pages);
+        // 取值不能小于1
+        page = Math.max(page, 1);
+
+        var skip = (page - 1) * limit;
+        for(var i = 1; i <= pages; i++){
+            a.push('{'+i+'}');
+        }
+        /*
+         * 1:升序
+         * -1:降序
+         * */
+        Content.find().sort({_id: -1}).limit(limit).skip(skip).populate("category").then(function(contents) {
+            res.render("admin/content_index",{
+                userInfo: req.userInfo,
+                contents: contents,
+                page: page,
+                a: a,
+                url: "/admin/content"
+            });
+        });
+    });
+});
+
+/*
+ * 内容添加
+ * */
+router.get("/content/add", function(req, res) {
+
+    Category.find().then(function(categories) {
+        res.render("admin/content_add", {
+            userInfo: req.userInfo,
+            categories: categories
+        });
+    })
+
+});
+
+/*
+* 内容保存
+* */
+router.post("/content/add", function(req, res) {
+
+    if(req.body.title == "") {
+        res.render("admin/error", {
+            userInfo: req.userInfo,
+            message: "内容标题不能为空"
+        });
+        return;
+    }
+
+    // 保存数据到数据库
+    new Content({
+        category: req.body.category,
+        title: req.body.title,
+        description: req.body.description,
+        content: req.body.content
+    }).save().then(function(rs) {
+        res.render("admin/error", {
+            userInfo: req.userInfo,
+            message: "内容保存成功",
+            url: '/admin/content'
+        });
+    });
+
+});
+
+/*
+* 修改内容
+* */
+router.get("/content/edit", function(req,res) {
+
+    var id= req.query.id || "";
+
+    var categories = [];
+
+    Category.find().then(function(rs) {
+
+        categories = rs;
+
+        return Content.findOne({
+            _id: id
+        }).populate("category");
+    }).then(function(content) {
+        if(!content) {
+            res.render("admin/error", {
+                userInfo: req.userInfo,
+                message: "指定内容不存在"
+            });
+            return Promise.reject();
+        } else {
+            res.render("admin/content_edit", {
+                userInfo: req.userInfo,
+                categories: categories,
+                content: content
+            });
+        }
+    })
+
+
+});
+
+/*
+ * 保存修改内容
+ * */
+router.post("/content/edit", function(req, res) {
+    var id = req.query.id || "";
+
+    if(req.body.title == "") {
+        res.render("admin/error", {
+            userInfo: req.userInfo,
+            message: "内容标题不能为空"
+        });
+        return;
+    }
+
+    Content.update({
+        _id: id
+    },{
+        category: req.body.category,
+        title: req.body.title,
+        description: req.body.description,
+        content: req.body.content
+    }).then(function(){
+        res.render("admin/success", {
+            userInfo: req.userInfo,
+            message: "内容保存成功",
+            url: "/admin/content"
+        });
+    })
+});
+
+/*
+* 内容删除
+* */
+router.get("/content/delete", function(req, res) {
+    var id = req.query.id || "";
+
+    Content.remove({
+        _id: id
+    }).then(function() {
+        res.render("admin/success", {
+            userInfo: req.userInfo,
+            message: "删除成功",
+            url: "/admin/content"
+        });
+    })
+});
 
 module.exports = router;
